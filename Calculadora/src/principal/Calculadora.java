a6bb652bb4d3eeb2b8d9ec5256742228d31316d4
ikaslea <ikaslea@HZ110802.zornotza.local>
2023-01-31 12:50:21 +0100

    calculadora_log

--- Calculadora/src/principal/Calculadora.java
+++ Calculadora/src/principal/Calculadora.java
@@ -1,16 +1,48 @@
 package principal;
 import menu.Menu;
 import operaciones.Operaciones;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Calculadora{
+	 private static final Logger LOGGER = Logger.getLogger(Calculadora.class.getName());
     public static void main(String[] args) {   
         int resultado = 0;
         String operacion = "";
         int[] operandos = new int [2];
         
+        
         Menu menu = new Menu();
         Operaciones operaciones = new Operaciones();
-       try { 
+    
+        Handler consoleHandler = null;
+        Handler fileHandler  = null;
+        try{
+            //Crear consoleHandler y fileHandler
+            consoleHandler = new ConsoleHandler();
+            fileHandler  = new FileHandler("./operaciones.log");
+             
+            //Asignar handlers al objeto LOGGER
+            LOGGER.setUseParentHandlers(false);
+            LOGGER.addHandler(consoleHandler);
+            LOGGER.addHandler(fileHandler);
+             
+            //Establecer niveles a handlers y LOGGER
+            consoleHandler.setLevel(Level.WARNING);
+            fileHandler.setLevel(Level.ALL);
+            LOGGER.setLevel(Level.ALL);
+             
+          
+			
+        }catch(IOException exception){
+            LOGGER.log(Level.SEVERE, "Ocurrió un error en FileHandler.", exception);
+        }
+        
+        
         do{
             operandos = menu.pedirNumeros();
             operacion = menu.menuOpciones();
@@ -25,19 +57,23 @@ public class Calculadora{
                 resultado = operaciones.multiplicar(operandos[0], operandos[1]);
                 System.out.println ("Resultado: " + resultado); 
             } else if (operacion.equalsIgnoreCase("/")){
+            	try{
                 resultado = operaciones.dividir(operandos[0], operandos[1]);
                 System.out.println ("Resultado: " + resultado);
-            } else if (operacion.equalsIgnoreCase("%")){
+                LOGGER.log(Level.FINE,"division, número 1"+ operandos[0]+"numero "+operandos[1]);
+            	}catch(ArithmeticException exception ){
+                    LOGGER.log(Level.WARNING, "division entre cero.",exception);
+                    System.out.println("ERROR");
+                }
+            }else if (operacion.equalsIgnoreCase("%")){
                 resultado = operaciones.resto(operandos[0], operandos[1]);
                 System.out.println ("Resultado: " + resultado);
             } else {
                 System.out.println ("Operaci�n no v�lida");
             }
         }   while (menu.repetir());
-       }catch ( ){
-    	   System.out.println("error aritmetico "+e.getMessage());
-    	   System.out.println("fin del programa");
-    	   
+        consoleHandler.close();
+        fileHandler.close();
+       
        }
     }
-}
